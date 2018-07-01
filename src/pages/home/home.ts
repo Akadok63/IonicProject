d1a2665c69d05a4c7f918b94726ae78a18e6d062
Varocha <valentin.mef@gmail.com>
2018-07-01 15:55:50 +0200

    Login OK

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  username:string;
+  password:string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {}
 
+  logIn() {
+    if(this.username == "root" && this.password == "toor") {
+      this.navCtrl.push(ListPage);
+    }else{
+      let alert = this.alertCtrl.create({
+        title: 'Wrong Id or Password',
+        subTitle: 'root/toor',
+        buttons: ['Compris']
+      });
+      alert.present();
+    }
   }
 
 }
